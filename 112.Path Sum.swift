6e139e1dcf26f8be6fb0aa869338dffb0d047c24
tdx-qinken <qinken547@gmail.com>
2018-01-31 11:22:58 +0800

    112. Path Sum

---
+++ 112.Path Sum.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  testSwitCommandLineTool
+//
+//  Created by tdx on 2018/1/29.
+//  Copyright © 2018年 sjjvenu. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Definition for a binary tree node.
+ */
+ public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+/*
+ */
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        guard root != nil else {
+            return false;
+        }
+        //如果是叶子结果，且传进来的sum为0，则找到所求值
+        if sum - root!.val == 0 && root?.left == nil && root?.right == nil{
+            return true;
+        }
+        //遍历时减去根的值，方便子节点对比
+        return hasPathSum(root!.left,sum-root!.val) || hasPathSum(root!.right,sum-root!.val);
+    }
+}
+
+let s = Solution();
+let root = TreeNode(5);let l1 = TreeNode(4);root.left = l1;let l2 = TreeNode(11);l1.left = l2;let l3 = TreeNode(7);l2.left = l3;let l4 = TreeNode(2);l2.right = l4;let r1 = TreeNode(8);root.right = r1;let r2 = TreeNode(13);r1.left = r2;let r3 = TreeNode(4);r1.right = r3;let r4 = TreeNode(1);r3.right = r4;
+print(s.hasPathSum(root,22))
